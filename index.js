92a116fa121ea94d9cba4e7cb7c89c9b923e361f
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-08-11 17:06:07 -0500

    finished first part of server

--- index.js
+++ index.js
@@ -20,11 +20,27 @@ app.route("/api/list")
     });
 
 app.route("/api/single/:id")
-    .get(function(req, res){
-        fs.readFile(jsonPath, "utf-8", function(err, file){
-            res.send(JSON.parse(file));
+    .get(function(req, res) {
+        fs.readFile(jsonPath, 'utf-8', function(err, fileContents) {
+            if (err) {
+                res.statusStatus(500);
+            } else {
+                var chunks = JSON.parse(fileContents);
+                var id = req.params.id;
+                var response;
+                chunks.forEach(function(chunk) {
+                    if (chunk.id === id) {
+                        response = chunk;
+                    }
+                });
+                if (response) {
+                    res.send(response);
+                } else {
+                    res.sendStatus(404);
+                }
+            }
         });
-    });
+    })
 
 app.listen(3000, function(){
     console.log("server is listening on port 3000");
